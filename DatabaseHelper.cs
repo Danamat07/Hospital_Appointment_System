5887753e74914ddf9e76cc6f97fabb0faaf83ea1
Matei_Dana <dana.matei@stud.ubbcluj.ro>
2025-02-21 00:10:45 +0200

    Created and Tested DataBase connection

---
+++ DatabaseHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Hospital_Appointment_System
+{
+    internal class DatabaseHelper
+    {
+        // connection string to connect to MySQL database
+        private static string connectionString = "server=localhost;database=HospitalDB;user=root;password=rootpassword;";
+
+        // method to get a MySQL connection
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        // method to execute query that returns data
+        // ex: SELECT
+        public static DataTable ExecuteQuery(string query)
+        {
+            // datatable to hold result set
+            DataTable dt = new DataTable();
+            using (var conn = GetConnection())  // open new connection
+            {
+                try
+                {
+                    conn.Open();    // open connection
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);   // create data adapter with query and connection
+                    adapter.Fill(dt);    // fill datatable with results from query
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error executing query: " + ex.Message);  // handle errors
+                }
+            }
+            return dt;  // return datatable with query results
+        }
+
+        // method to execute query that doesn't return data
+        // ex: INSERT, UPDATE, DELETE
+        public static void ExecuteNonQuery(string query)
+        {
+            using (var conn = GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error executing non-query: " + ex.Message);
+                }
+            }
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Hospital_Appointment_System
 {
@@ -15,6 +16,21 @@ namespace Hospital_Appointment_System
         public Form1()
         {
             InitializeComponent();
+            Form1_Load(this, EventArgs.Empty);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // query to fetch data from Test table
+            string query = "SELECT * FROM Test";
+
+            // call DatabaseHelper to execute the query
+            DataTable dt = DatabaseHelper.ExecuteQuery(query);
+
+            // bind the fetched data to DataGridView
+            dataGridView1.DataSource = dt;
+
+
         }
     }
 }
