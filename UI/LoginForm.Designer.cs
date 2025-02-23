f1787f32b7b8efee7a61b2793b7e160b7e952497
Matei_Dana <marydanamat@gmail.com>
2025-02-23 20:07:15 +0200

    Login Form Design

--- UI/LoginForm.Designer.cs
+++ UI/LoginForm.Designer.cs
@@ -62,7 +62,7 @@
             // 
             this.btnPatientLogin.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btnPatientLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPatientLogin.Location = new System.Drawing.Point(185, 255);
+            this.btnPatientLogin.Location = new System.Drawing.Point(196, 248);
             this.btnPatientLogin.Name = "btnPatientLogin";
             this.btnPatientLogin.Size = new System.Drawing.Size(75, 29);
             this.btnPatientLogin.TabIndex = 5;
@@ -72,15 +72,16 @@
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(50, 167);
+            this.txtPassword.Location = new System.Drawing.Point(50, 183);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(161, 22);
             this.txtPassword.TabIndex = 4;
             // 
             // txtEmail
             // 
             this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEmail.Location = new System.Drawing.Point(50, 92);
+            this.txtEmail.Location = new System.Drawing.Point(50, 124);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(278, 22);
             this.txtEmail.TabIndex = 3;
@@ -90,7 +91,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(47, 146);
+            this.label3.Location = new System.Drawing.Point(47, 164);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 16);
             this.label3.TabIndex = 2;
@@ -100,7 +101,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(47, 71);
+            this.label2.Location = new System.Drawing.Point(47, 105);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 16);
             this.label2.TabIndex = 1;
@@ -110,7 +111,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(192, 19);
+            this.label1.Location = new System.Drawing.Point(192, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 24);
             this.label1.TabIndex = 0;
