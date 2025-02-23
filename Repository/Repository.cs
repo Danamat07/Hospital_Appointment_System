48c17510c9c444fe6c4dd304960b683cf90dddd4
Matei_Dana <marydanamat@gmail.com>
2025-02-23 16:53:56 +0200

    Create Patient Repository

--- Repository/Repository.cs
+++ Repository/Repository.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using Hospital_Appointment_System.Domain;
+using System.Runtime.CompilerServices;
 
 namespace Hospital_Appointment_System.Repository
 {
@@ -21,4 +23,101 @@ namespace Hospital_Appointment_System.Repository
             return new MySqlCommand(query, connection);
         }
     }
+
+    public class PatientRepository
+    {
+        public List<Patient> GetAllPatients()
+        {
+            var patients = new List<Patient>();
+            string query = "SELECT * FROM Patient";
+            using (var connection = DatabaseHelper.GetConnection())
+            {
+                connection.Open();
+                MySqlCommand cmd = DatabaseHelper.CreateCommand(query, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    patients.Add(new Patient(
+                        reader.GetInt32("ID"),
+                        reader.GetString("Name"),
+                        reader.GetString("Email"),
+                        reader.GetString("Phone"),
+                        reader.GetString("Password")
+                        ));
+                }
+            }
+            return patients;
+        }
+
+        public Patient GetPatientById(int id)
+        {
+            Patient patient = null;
+            string query = "SELECT * FROM Patient WHERE ID @Id";
+
+            using (var connection = DatabaseHelper.GetConnection())
+            {
+                connection.Open();
+                MySqlCommand cmd = DatabaseHelper.CreateCommand(query, connection);
+                cmd.Parameters.AddWithValue("@Id", id);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    patient = new Patient(
+                        reader.GetInt32("ID"),
+                        reader.GetString("Name"),
+                        reader.GetString("Email"),
+                        reader.GetString("Phone"),
+                        reader.GetString("Password")
+                        );
+                }
+            }
+            return patient;
+        }
+
+        public void AddPatient(Patient patient)
+        {
+            string query = "INSERT INTO Patient (Name, Email, Phone, Password) VALUES (@Name, @Email, @Phone, @Password)";
+            using (var connection = DatabaseHelper.GetConnection())
+            {
+                connection.Open();
+                MySqlCommand cmd = DatabaseHelper.CreateCommand(query, connection);
+                cmd.Parameters.AddWithValue("@Name", patient.Name);
+                cmd.Parameters.AddWithValue("@Email", patient.Email);
+                cmd.Parameters.AddWithValue("@Phone", patient.Phone);
+                cmd.Parameters.AddWithValue("@Password", patient.Password);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdatePatient(Patient patient)
+        {
+            string query = "UPDATE Patient SET Name = @Name, Email = @Email, Phone = @Phone, Password = @Password WHERE ID = @Id";
+            using (var connection = DatabaseHelper.GetConnection())
+            {
+                connection.Open();
+                MySqlCommand cmd = DatabaseHelper.CreateCommand(query, connection);
+                cmd.Parameters.AddWithValue("@Id", patient.Id);
+                cmd.Parameters.AddWithValue("@Name", patient.Name);
+                cmd.Parameters.AddWithValue("@Email", patient.Email);
+                cmd.Parameters.AddWithValue("@Phone", patient.Phone);
+                cmd.Parameters.AddWithValue("@Password", patient.Password);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeletePatient(int id)
+        {
+            string query = "DELETE FROM Patient WHERE ID = @Id";
+            using (var connection = DatabaseHelper.GetConnection())
+            {
+                connection.Open();
+                MySqlCommand cmd = DatabaseHelper.CreateCommand(query, connection);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
 }
