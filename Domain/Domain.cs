8c63d77051e429e8ea171e9238ab128d36866391
Matei_Dana <dana.matei@stud.ubbcluj.ro>
2025-02-23 14:53:13 +0200

    Add project leyers

---
+++ Domain/Domain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Appointment_System.Domain
+{
+    internal class Pacient
+    {
+
+    }
+
+    internal class Doctor
+    {
+
+    }
+
+    internal class Appointment
+    {
+
+    }
+}
