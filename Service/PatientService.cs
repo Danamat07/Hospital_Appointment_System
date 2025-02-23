9ee9414cf42bac75b0fab2d11eea1fe57fda0cd0
Matei_Dana <marydanamat@gmail.com>
2025-02-23 17:48:44 +0200

    Create Patient Service

---
+++ Service/PatientService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital_Appointment_System.Domain;
+using Hospital_Appointment_System.Repository;
+
+namespace Hospital_Appointment_System.Service
+{
+    public class PatientService
+    {
+        private readonly PatientRepository patientRepository;
+
+        public PatientService()
+        {
+            patientRepository = new PatientRepository();
+        }
+
+        public List<Patient> GetAllPatients()
+        {
+            try
+            {
+                return patientRepository.GetAllPatients();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while retrieving patients.", ex);
+            }
+        }
+
+        public Patient GetPatientById(int id)
+        {
+            try
+            {
+                return patientRepository.GetPatientById(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while retrieving the patient with ID {id}.", ex);
+            }
+        }
+
+        public void AddPatient(Patient patient)
+        {
+            try
+            {
+                patientRepository.AddPatient(patient);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while adding the patient.", ex);
+            }
+        }
+
+        public void UpdatePatient(Patient patient)
+        {
+            try
+            {
+                patientRepository.UpdatePatient(patient);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while updating the patient with ID {patient.Id}.", ex);
+            }
+        }
+
+        public void DeletePatient(int id)
+        {
+            try
+            {
+                patientRepository.DeletePatient(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while deleting the patient with ID {id}.", ex);
+            }
+        }
+    }
+}
