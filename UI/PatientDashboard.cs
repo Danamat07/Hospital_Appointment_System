16b022f4e4a0833d6b2e50dc4d9a1fdff2a54d89
Matei_Dana <marydanamat@gmail.com>
2025-03-01 14:38:20 +0200

    Fix method for DataGridView display

--- UI/PatientDashboard.cs
+++ UI/PatientDashboard.cs
@@ -28,11 +28,11 @@ namespace Hospital_Appointment_System.UI
         {
             if (patientTabs.SelectedTab == tabViewAppontments)
             {
-                LoadAppointmentsForPatient(patient.Id);
+                LoadAppointmentsForPatient(patient.Id, dgvUpcomingAppointments);
             }
             else if (patientTabs.SelectedTab == tabAddAppointment)
             {
-                ScheduleNewAppointment();
+                LoadAppointmentsForPatient(patient.Id, dgvAddAppointment);
             }
             else if (patientTabs.SelectedTab == tabUpdateAppointment)
             {
@@ -44,36 +44,36 @@ namespace Hospital_Appointment_System.UI
             }
         }
 
-        ScheduleNewAppointment()
-        {
-            try
-            {
-                // Get selected date
-                DateTime selectedDate = dtpAppointmentDate.Value.Date;
-                // Get selected time
-                DateTime selectedTime = dtpAppointmentTime.Value;
-                // Combine date and time into one DateTime object
-                DateTime appointmentStartDateTime = new DateTime(
-                    selectedDate.Year, selectedDate.Month, selectedDate.Day,
-                    selectedTime.Hour, selectedTime.Minute, 0
-                );
-                int patientId = patient.Id;
-                // -------------------------------------------------------------------------
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error: {ex.Message}");
-            }
-        }
+        //ScheduleNewAppointment()
+        //{
+        //    try
+        //    {
+        //        // Get selected date
+        //        DateTime selectedDate = dtpAppointmentDate.Value.Date;
+        //        // Get selected time
+        //        DateTime selectedTime = dtpAppointmentTime.Value;
+        //        // Combine date and time into one DateTime object
+        //        DateTime appointmentStartDateTime = new DateTime(
+        //            selectedDate.Year, selectedDate.Month, selectedDate.Day,
+        //            selectedTime.Hour, selectedTime.Minute, 0
+        //        );
+        //        int patientId = patient.Id;
+        //        // -------------------------------------------------------------------------
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Error: {ex.Message}");
+        //    }
+        //}
 
-        private void LoadAppointmentsForPatient(int patientId)
+        private void LoadAppointmentsForPatient(int patientId, DataGridView dgv)
         {
             try
             {
                 // get the list of appointments from the controller
                 var appointments = appointmentController.GetAppointmentsByPatientId(patientId);
                 // bind the appointments list to the DataGridView
-                dgvUpcomingAppointments.DataSource = appointments;
+                dgv.DataSource = appointments;
             }
             catch (Exception ex)
             {
